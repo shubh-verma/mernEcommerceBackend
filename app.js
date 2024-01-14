b7a8b80e3f284c5b6b30e430b226b9964b5d81f3
shubh-verma <shubhvcs@gmail.com>
2024-01-14 21:33:54 +0530

    [add] get path for connection

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
 const port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+  http = require("http"),
+  fs = require("fs"),
+  html = fs.readFileSync("index.html");
 
-const log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+const log = function (entry) {
+  fs.appendFileSync(
+    "/tmp/sample-app.log",
+    new Date().toISOString() + " - " + entry + "\n"
+  );
 };
 
 const server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        let body = '';
+  if (req.method === "POST") {
+    let body = "";
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+    req.on("data", function (chunk) {
+      body += chunk;
+    });
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
+    req.on("end", function () {
+      if (req.url === "/") {
+        log("Received message: " + body);
+      } else if ((req.url = "/scheduled")) {
+        log(
+          "Received task " +
+            req.headers["x-aws-sqsd-taskname"] +
+            " scheduled at " +
+            req.headers["x-aws-sqsd-scheduled-at"]
+        );
+      }
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+      res.writeHead(200, "OK", { "Content-Type": "text/plain" });
+      res.end();
+    });
+  } else {
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+  }
+});
+
+server.use(cors());
+
+server.get("/getData", (req, res) => {
+  res.send("Hello from backend!");
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
 
 // Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+console.log("Server running at http://127.0.0.1:" + port + "/");
