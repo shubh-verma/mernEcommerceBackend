835324e1b4e1abb873feddf43aa28f15d89db46a
shubh-verma <shubhvcs@gmail.com>
2024-01-15 01:12:39 +0530

    [add] cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hi from backend, kya ye chalega!");
+  res.send("Hi from backend, kya ye chalega! yes chal gaya");
 });
 
 app.listen(port, () => {
